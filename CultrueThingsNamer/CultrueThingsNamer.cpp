d3e7d09e99a135feb1e0fe2f24164b7e92fea2be
Jingling <a133022001@gmail.com>
2024-12-31 21:54:41 +0800

    24/12/31 update

--- CultrueThingsNamer/CultrueThingsNamer.cpp
+++ CultrueThingsNamer/CultrueThingsNamer.cpp
@@ -1,12 +1,12 @@
 #include "CultrueThingsNamer.h"
-#include "QCoreApplication";
-#include "QFile";
-#include "QtextStream";
-#include "QString";
-#include "QList";
-#include "QDebug";
-#include "qmessagebox";
-#include "QRegularExpression";
+#include "QCoreApplication"
+#include "QFile"
+#include "QtextStream"
+#include "QString"
+#include "QList"
+#include "QDebug"
+#include "qmessagebox"
+#include "QRegularExpression"
 
 #include < QJsonObject > 
 #include < QJsonArray >
@@ -16,6 +16,7 @@
 
 #include<QRandomGenerator>
 #include<QClipboard>
+#include <QIcon>
 
 
 QString rulesPath = "./rules/totalRules.json";
@@ -32,6 +33,11 @@ QList<QString> generalRuleElementsList;
 QList<QString> spawnedNameCN;
 QList<QString> spawnedNameEN;
 
+QString LE_SpawnMode_1 = "CN";
+QString LE_SpawnMode_2 = "CN";
+QString LE_SpawnMode_3 = "CN";
+
+bool lineInputRequired_1 = false, lineInputRequired_2 = false, lineInputRequired_3 = false;
 
 
 
@@ -43,7 +49,7 @@ QJsonDocument static loadFromJSONS(QString path)
     {
 
         QMessageBox msgbox;
-        msgbox.setIcon(QMessageBox::Warning);
+        msgbox.setIcon(QMessageBox::Critical);
         msgbox.setText("file open failed:" + file.errorString());
         msgbox.exec();
 
@@ -80,6 +86,8 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    setWindowIcon(QIcon("./CultrueThingsNamer.ico"));
+    setWindowTitle(UIStringTable.value("title_CultureThingNamer").toString());
 
     generalCopyOptn.append(UIStringTable.value("gnlCpyOptn_CN").toString());
     generalCopyOptn.append(UIStringTable.value("gnlCpyOptn_CNWS").toString());
@@ -94,11 +102,32 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
     ui.rmpstab_button_copyOptn->setText(UIStringTable.value("bnCopy").toString());
     ui.rmcanmtab_button_CopyOptn->setText(UIStringTable.value("bnCopy").toString());
 
+    ui.rmpstab_ENMode_checkBox_1->setText(UIStringTable.value("ckBoxENMode").toString());
+    ui.rmpstab_ENMode_checkBox_2->setText(UIStringTable.value("ckBoxENMode").toString());
+    ui.rmpstab_ENMode_checkBox_3->setText(UIStringTable.value("ckBoxENMode").toString());
+
     ui.rmstab_comboBox_CopyPhaseOptn->addItems(generalCopyOptn);
     ui.rmcanmtab_comboBox_CopyPhaseOptn->addItems(generalCopyOptn);
 
+    QFont settedFont;
+    settedFont.setPointSizeF(30);
+
     ui.rmstab_result_textBrowser->setFontPointSize(30);
     ui.rmcanmtab_result_textBrowser->setFontPointSize(30);
+
+    
+
+    ui.rmpstab_result_lineEd_1->setFont(settedFont);
+    ui.rmpstab_result_lineEd_2->setFont(settedFont);
+    ui.rmpstab_result_lineEd_3->setFont(settedFont);
+
+    ui.rmpstab_result_lineEd_1->setReadOnly(true);
+    ui.rmpstab_result_lineEd_2->setReadOnly(true);
+    ui.rmpstab_result_lineEd_3->setReadOnly(true);
+
+    LE_SpawnMode_1 = "CN";
+    LE_SpawnMode_2 = "CN";
+    LE_SpawnMode_3 = "CN";
 
     
     for (auto rootobj : ruleJsonObj)
@@ -108,7 +137,10 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
         generalCultureListCN.append(rootobj.toObject().value("CHName").toString());
     }
     ui.rmstab_comboBox_CultureOptn->addItems(generalCultureListCN);
+    ui.rmpstab_comboBox_CultureOptn->addItems(generalCultureListCN);
     ui.rmcanmtab_comboBox_CultureOptn->addItems(generalCultureListCN);
+
+    /*------------------------------------------------------------------(P1)-----------------------------------------------------------*/
 
     connect
     (ui.rmstab_comboBox_CultureOptn, &QComboBox::currentIndexChanged, this, [=]
@@ -168,19 +200,34 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
                         }
                         else
                         {
-                            if (at.lastIndexOf(",") != -1)//Has OR
+                            try
                             {
-                                int tgtrmIndex = QRandomGenerator::global()->bounded(1, at.split(",").size() - 1);
-                                QString tgt = at.split(",")[tgtrmIndex < 0 ? 0 : tgtrmIndex];
-                                tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(tgt).toArray();
+                                if (at.lastIndexOf(",") != -1)//Has OR
+                                {
+                                    int tgtrmIndex = QRandomGenerator::global()->bounded(1, at.split(",").size() - 1);
+                                    QString tgt = at.split(",")[tgtrmIndex < 0 ? 0 : tgtrmIndex];
+                                    tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(tgt).toArray();
+                                }
+                                else
+                                {
+                                    tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(at).toArray();
+                                }
+
+                                int rmNum = QRandomGenerator::global()->bounded(0, tgtArray.size() - 1);
+                                spawnedNameEN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("EN").toString());
+                                spawnedNameCN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("CN").toString());
                             }
-                            else
+                            catch (const std::runtime_error& e)
                             {
-                                tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(at).toArray();
+                                QMessageBox msgbox;
+                                msgbox.setIcon(QMessageBox::Critical);
+                                msgbox.setText(e.what());
+                                msgbox.exec();
                             }
-                            int rmNum = QRandomGenerator::global()->bounded(0, tgtArray.size() - 1);
-                            spawnedNameEN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("EN").toString());
-                            spawnedNameCN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("CN").toString());
+
+
+
+                            
                         }
 
                     }
@@ -202,7 +249,7 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
                 else
                 {
                     QMessageBox msgbox;
-                    msgbox.setIcon(QMessageBox::Warning);
+                    msgbox.setIcon(QMessageBox::Critical);
                     msgbox.setText("error: rule stringTable not match");
                     msgbox.exec();
                 }
@@ -210,7 +257,7 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
             else
             {
                 QMessageBox msgbox;
-                msgbox.setIcon(QMessageBox::Warning);
+                msgbox.setIcon(QMessageBox::Critical);
                 msgbox.setText("error: this culture haven't name-build up elements yet");
                 msgbox.exec();
             }
@@ -270,7 +317,7 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
             else
             {
                 QMessageBox msgbox;
-                msgbox.setIcon(QMessageBox::Warning);
+                msgbox.setIcon(QMessageBox::Critical);
                 msgbox.setText("No Result Existing!");
                 msgbox.exec();
             }
@@ -278,7 +325,153 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
         }
     );
     /*
-    -------------------------------------------------------------------------------------------------------------------------
+    ---------------------------------------------------(P2--)------------------------------------------------------------------
+    */
+    connect(ui.rmpstab_comboBox_CultureOptn, &QComboBox::currentIndexChanged, this, [=] 
+        {
+            generalRuleNameList.clear();
+            generalRuleElementsList.clear();
+            QString tgtENName = generalCultureListREGName[ui.rmpstab_comboBox_CultureOptn->currentIndex()];
+
+            QJsonObject currentJsonObject = elementsJsonObj.value(tgtENName).toObject();
+            QStringList elementsList = currentJsonObject.keys();
+            for (auto crtKeys : elementsList)
+            {
+                
+                generalRuleNameList.append(crtKeys);
+                generalRuleElementsList.append(crtKeys);
+            }
+            generalRuleNameList.append("Input");
+            generalRuleElementsList.append("Input");
+
+            ui.rmpstab_result_comboBox_1->clear();
+            ui.rmpstab_result_comboBox_2->clear();
+            ui.rmpstab_result_comboBox_3->clear();
+            ui.rmpstab_result_comboBox_1->addItems(generalRuleNameList);
+            ui.rmpstab_result_comboBox_2->addItems(generalRuleNameList);
+            ui.rmpstab_result_comboBox_3->addItems(generalRuleNameList);
+            /*
+            ui.rmstab_result_textBrowser->append("/n");
+            for (auto s : ruleList)
+            {
+                ui.rmstab_result_textBrowser->append(s);
+            }
+            */
+        });
+
+    connect(ui.rmpstab_result_comboBox_1, &QComboBox::currentIndexChanged, this, [=] 
+        {
+            lineInputRequired_1 = (ui.rmpstab_result_comboBox_1->currentText() == "Input");
+            ui.rmpstab_result_lineEd_1->setReadOnly(!lineInputRequired_1);
+        });
+    connect(ui.rmpstab_result_comboBox_2, &QComboBox::currentIndexChanged, this, [=] 
+        {
+            lineInputRequired_2 = (ui.rmpstab_result_comboBox_2->currentText() == "Input");
+            ui.rmpstab_result_lineEd_2->setReadOnly(!lineInputRequired_2);
+        });
+    connect(ui.rmpstab_result_comboBox_3, &QComboBox::currentIndexChanged, this, [=] 
+        {
+            lineInputRequired_3 = (ui.rmpstab_result_comboBox_3->currentText() == "Input");
+            ui.rmpstab_result_lineEd_3->setReadOnly(!lineInputRequired_3);
+        });
+
+    connect(ui.rmpstab_ENMode_checkBox_1, &QCheckBox::checkStateChanged, this, [=] 
+        {
+            LE_SpawnMode_1 = ui.rmpstab_ENMode_checkBox_1->isChecked() ? "EN" : "CN";  
+        });
+
+    connect(ui.rmpstab_ENMode_checkBox_2, &QCheckBox::checkStateChanged, this, [=]
+        {
+            LE_SpawnMode_2 = ui.rmpstab_ENMode_checkBox_2->isChecked() ? "EN" : "CN";
+        });
+
+    connect(ui.rmpstab_ENMode_checkBox_3, &QCheckBox::checkStateChanged, this, [=]
+        {
+            LE_SpawnMode_3 = ui.rmpstab_ENMode_checkBox_3->isChecked() ? "EN" : "CN";
+        });
+
+    connect(ui.rmpstab_button_spawnOptn, &QPushButton::clicked, this, [=] 
+        {
+
+                QJsonObject crtAdresElementJson = elementsJsonObj.value(generalCultureListREGName[ui.rmpstab_comboBox_CultureOptn->currentIndex()]).toObject();
+
+
+
+
+            if ( !(lineInputRequired_1 || lineInputRequired_2 || lineInputRequired_3))
+            {
+                
+                QList<QJsonArray> rmElement_JSON_Adres;
+                rmElement_JSON_Adres.append( crtAdresElementJson.value(generalRuleElementsList[ui.rmpstab_result_comboBox_1->currentIndex()]).toArray());
+                rmElement_JSON_Adres.append( crtAdresElementJson.value(generalRuleElementsList[ui.rmpstab_result_comboBox_2->currentIndex()]).toArray());
+                rmElement_JSON_Adres.append( crtAdresElementJson.value(generalRuleElementsList[ui.rmpstab_result_comboBox_3->currentIndex()]).toArray());
+
+                QList<int> rmIndexForElements;
+                for (auto Adres : rmElement_JSON_Adres)
+                {
+                    int i = QRandomGenerator::global()->bounded(0, Adres.size()) - 1;
+                    i = i < 0 ? 0 : i;
+                    rmIndexForElements.append(i);
+
+                }
+                
+                ui.rmpstab_result_lineEd_1->setText(rmElement_JSON_Adres[0].at(rmIndexForElements[0]).toObject().value(LE_SpawnMode_1).toString());
+                ui.rmpstab_result_lineEd_2->setText(rmElement_JSON_Adres[1].at(rmIndexForElements[1]).toObject().value(LE_SpawnMode_2).toString());
+                ui.rmpstab_result_lineEd_3->setText(rmElement_JSON_Adres[2].at(rmIndexForElements[2]).toObject().value(LE_SpawnMode_3).toString());
+
+            }
+            else
+            {
+
+                if (!lineInputRequired_1)
+                {
+                    QJsonArray rmElement_JSON_Adres_1 = crtAdresElementJson.value(generalRuleElementsList[ui.rmpstab_result_comboBox_1->currentIndex()]).toArray();
+                    int rmIndexForElement_1 = QRandomGenerator::global()->bounded(0, rmElement_JSON_Adres_1.size()) - 1;
+                    rmIndexForElement_1 = rmIndexForElement_1 < 0 ? 0 : rmIndexForElement_1;
+                    ui.rmpstab_result_lineEd_1->setText(rmElement_JSON_Adres_1[rmIndexForElement_1].toObject().value(LE_SpawnMode_1).toString());
+
+                }
+                if (!lineInputRequired_2)
+                {
+                    QJsonArray rmElement_JSON_Adres_2 = crtAdresElementJson.value(generalRuleElementsList[ui.rmpstab_result_comboBox_2->currentIndex()]).toArray();
+                    int rmIndexForElement_2 = QRandomGenerator::global()->bounded(0, rmElement_JSON_Adres_2.size()) - 1;
+                    rmIndexForElement_2 = rmIndexForElement_2 < 0 ? 0 : rmIndexForElement_2;
+                    ui.rmpstab_result_lineEd_2->setText(rmElement_JSON_Adres_2[rmIndexForElement_2].toObject().value(LE_SpawnMode_2).toString());
+                }
+                if (!lineInputRequired_3)
+                {
+                    QJsonArray rmElement_JSON_Adres_3 = crtAdresElementJson.value(generalRuleElementsList[ui.rmpstab_result_comboBox_3->currentIndex()]).toArray();
+                    int rmIndexForElement_3 = QRandomGenerator::global()->bounded(0, rmElement_JSON_Adres_3.size()) - 1;
+                    rmIndexForElement_3 = rmIndexForElement_3 < 0 ? 0 : rmIndexForElement_3;
+                    ui.rmpstab_result_lineEd_3->setText(rmElement_JSON_Adres_3[rmIndexForElement_3].toObject().value(LE_SpawnMode_3).toString());
+                }
+                
+
+            }
+
+
+
+
+        });
+    connect(ui.rmpstab_button_copyOptn, &QPushButton::clicked, this, [=] 
+        {
+            QClipboard* cb = QApplication::clipboard();
+            QString Result;
+            Result.append(ui.rmpstab_result_lineEd_1->text());
+            Result.append(ui.rmpstab_result_lineEd_2->text());
+            Result.append(ui.rmpstab_result_lineEd_3->text());
+            cb->setText(Result);
+
+        });
+
+
+
+
+
+
+
+    /*
+    ---------------------------------------------------(P3  )-------------------------------------------------------------------
     */
 
     connect(ui.rmcanmtab_comboBox_CultureOptn, &QComboBox::currentIndexChanged, this, [=]
@@ -323,19 +516,33 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
                         }
                         else
                         {
-                            if (at.lastIndexOf(",") != -1)//Has OR
+
+                            try
                             {
-                                int tgtrmIndex = QRandomGenerator::global()->bounded(0, at.split(",").size() - 1);
-                                QString tgt = at.split(",")[tgtrmIndex < 0 ? 0 : tgtrmIndex];
-                                tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(tgt).toArray();
+                                if (at.lastIndexOf(",") != -1)//Has OR
+                                {
+                                    int tgtrmIndex = QRandomGenerator::global()->bounded(0, at.split(",").size() - 1);
+                                    QString tgt = at.split(",")[tgtrmIndex < 0 ? 0 : tgtrmIndex];
+                                    tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(tgt).toArray();
+                                }
+                                else
+                                {
+                                    tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(at).toArray();
+                                }
+                                int rmNum = QRandomGenerator::global()->bounded(0, tgtArray.size() - 1);
+                                spawnedNameEN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("EN").toString());
+                                spawnedNameCN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("CN").toString());
                             }
-                            else
+                            catch (const std::runtime_error& e)
                             {
-                                tgtArray = elementsJsonObj.value(generalCultureListREGName[indexInCulture]).toObject().value(at).toArray();
+                                QMessageBox msgbox;
+                                msgbox.setIcon(QMessageBox::Critical);
+                                msgbox.setText(e.what());
+                                msgbox.exec();
                             }
-                            int rmNum = QRandomGenerator::global()->bounded(0, tgtArray.size() - 1);
-                            spawnedNameEN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("EN").toString());
-                            spawnedNameCN.append(tgtArray[rmNum < 0 ? 0 : rmNum].toObject().value("CN").toString());
+
+
+
                         }
 
                     }
@@ -356,7 +563,7 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
                 else
                 {
                     QMessageBox msgbox;
-                    msgbox.setIcon(QMessageBox::Warning);
+                    msgbox.setIcon(QMessageBox::Critical);
                     msgbox.setText("error: rule stringTable not match");
                     msgbox.exec();
                 }
@@ -364,7 +571,7 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
             else
             {
                 QMessageBox msgbox;
-                msgbox.setIcon(QMessageBox::Warning);
+                msgbox.setIcon(QMessageBox::Critical);
                 msgbox.setText("error: this culture haven't name-build up elements yet");
                 msgbox.exec();
             }
@@ -417,7 +624,7 @@ CultrueThingsNamer::CultrueThingsNamer(QWidget *parent)
             else
             {
                 QMessageBox msgbox;
-                msgbox.setIcon(QMessageBox::Warning);
+                msgbox.setIcon(QMessageBox::Critical);
                 msgbox.setText("No Result Existing!");
                 msgbox.exec();
             }
